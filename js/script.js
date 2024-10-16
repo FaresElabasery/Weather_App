41777bad5afd020f95988bae3012c25ada121930
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2024-10-17 00:17:45 +0300

    fix : fix issue with view weather photo

--- js/script.js
+++ js/script.js
@@ -21,19 +21,19 @@ async function checkWeather(city) {
         document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
         document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
         if (data.weather[0].main == 'Clear')
-            weatherIcon.src = '/images/clear.png';
+            weatherIcon.src = 'images/clear.png';
         else if (data.weather[0].main == 'Clouds')
-            weatherIcon.src = '/images/clouds.png';
+            weatherIcon.src = 'images/clouds.png';
         else if (data.weather[0].main == 'Drizzle')
-            weatherIcon.src = '/images/drizzle.png';
+            weatherIcon.src = 'images/drizzle.png';
         else if (data.weather[0].main == 'Rain')
-            weatherIcon.src = '/images/rain.png';
+            weatherIcon.src = 'images/rain.png';
         else if (data.weather[0].main == 'Snow')
-            weatherIcon.src = '/images/snow.png';
+            weatherIcon.src = 'images/snow.png';
         else if (data.weather[0].main == 'Mist')
-            weatherIcon.src = '/images/mist.png';
+            weatherIcon.src = 'images/mist.png';
         else if (data.weather[0].main == 'Wind')
-            weatherIcon.src = '/images/wind.png';
+            weatherIcon.src = 'images/wind.png';
         else
             weatherIcon.src = '/images/clear.png';
         console.log(data.weather[0].main);
